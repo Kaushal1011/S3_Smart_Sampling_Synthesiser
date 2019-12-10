a583978d0edd5e18e59684748eaf3541c7d4a67c
Kaushal1011 <kaushalpatil10@gmail.com>
2019-12-11 00:50:26 +0530

    edits

--- S3SynthMain.py
+++ S3SynthMain.py
@@ -3,7 +3,7 @@
 # import keyboard
 from dependency import np, pd, sp
 from S3Synth import S3Synth
-from S3Utils import freq_calc, find_Ns, get_note, find_maxsig, make_octaves
+from S3Utils import freq_calc, find_Ns, get_note, find_maxsig, make_octaves, freq_from_HPS,freq_from_autocorr,freq_from_autocorr,freq_from_crossings,freq_from_fft
 from S3DataUtils import train_S3, create_FunctionFrame
 from matplotlib import pyplot as plt
 # from scipy.io.wavfile import write
@@ -19,35 +19,50 @@ class S3App:
         """Loads a Sample into the synthesiser"""
         self.Ss, self.wave = sp.io.wavfile.read(file_path, mmap=False)
         self.wave=np.array(self.wave[:, 0])
+        plt.plot(self.wave[:])
+        plt.show()
+        print("Enter indexes in which wave is sustained")
+        a=input()
+        b=input()
+        self.waves=self.wave[int(a):int(b)]
+
 
     def load_trainedsynth(self):
         """Loads all properties of S3 trains S3 and initialises S3Synth"""
-        
-        freq = freq_calc(self.wave, self.Ss)
-        self.Ss = int(self.Ss)
-        self.freq, self.note = get_note(freq)
-        self.Ns = int(find_Ns(self.freq, self.Ss))
-        self.Fs = int(freq)
-        print(freq, self.Ns, self.Ss, self.note)
-        self.wave_sampled = find_maxsig(self.wave, self.Ns)
-        
-        # print(self.env.Ns)
-        func_frame = create_FunctionFrame(self.Fs, self.Ns, self.Ss)
-        self.reg = train_S3(func_frame, self.wave_sampled)
-        print(self.reg.score(func_frame, self.wave_sampled))
-        
 
-    
+        freq = freq_calc(self.waves, self.Ss)
+        freq1= freq_from_HPS(self.waves,self.Ss)
+        freq2= freq_from_autocorr(self.waves,self.Ss)
+        freq3 = freq_from_fft(self.waves,self.Ss)
+        freq4 = freq_from_crossings(self.waves,self.Ss)
+        print("freq ",freq3,"freqfft ",freq3,"freqautocorr ",freq2,"freqhps ",freq1,"freqcross ",freq4)
+        self.Ss = int(self.Ss)
+        print("Enter Frequency")
+        fs=int(input())
+        self.freq, self.note = get_note(fs)
+        self.Ns = int(find_Ns(self.freq, self.Ss))
+        self.Fs = int(fs)
+        print(freq, self.Ns, self.Ss, self.note)
+        self.waves = self.waves
+
+        # print(self.env.Ns)
+        func_frame = create_FunctionFrame(self.Fs, len(self.waves), self.Ss)
+        print(func_frame.shape)
+        self.reg = train_S3(func_frame, self.waves)
+        print(self.reg.score(func_frame, self.waves))
+
+
+
 
 
 def main():
     kaypee = S3App()
-    kaypee.load_file('flute1.wav')
+    kaypee.load_file('Samples/ElectricGuitar.wav')
     kaypee.load_trainedsynth()
     s = Server()
     s.setMidiInputDevice(99) # Open all input devices.
     s.boot()
-    
+
     Synthesiser=S3Synth(kaypee.reg.coef_)
     a=Synthesiser.out()
     s.gui(locals())
--- S3Utils.py
+++ S3Utils.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python3
 """Utils Functions for S3 Synthesiser App"""
-
+from __future__ import division
 from typing import Tuple
 
 from scipy.interpolate import interp1d
 
 from dependency import np
 
+from numpy.fft import rfft
+from numpy import argmax, mean, diff, log, nonzero
+from scipy.signal import  correlate
+from scipy import signal
+from time import time
+import sys
+import soundfile as sf
+
+from parabolic import parabolic
+
 
 def freq_calc(sig: np.ndarray, Ss: int) -> float:
     """Calculates the average frequency of the input signal (of a recorded note)"""
     rep = 0
-    for i in range(int(len(sig) / 3), int(2 * len(sig) / 3)):
+    for i in range(len(sig)-1):
         if sig[i + 1] >= 0 and sig[i] <= 0:
             rep += 1
     return Ss / (len(sig) / (3 * rep))
@@ -105,6 +115,93 @@ def find_maxsig(sig: np.ndarray, Ns: int) -> np.ndarray:
             sig_ret = sig[i * Ns:(i + 1) * Ns]
 
     return sig_ret
+
+def freq_from_crossings(sig, fs):
+    """
+    Estimate frequency by counting zero crossings
+    """
+    # Find all indices right before a rising-edge zero crossing
+    indices = nonzero((sig[1:] >= 0) & (sig[:-1] < 0))[0]
+
+    # Naive (Measures 1000.185 Hz for 1000 Hz, for instance)
+    # crossings = indices
+
+    # More accurate, using linear interpolation to find intersample
+    # zero-crossings (Measures 1000.000129 Hz for 1000 Hz, for instance)
+    crossings = [i - sig[i] / (sig[i+1] - sig[i]) for i in indices]
+
+    # Some other interpolation based on neighboring points might be better.
+    # Spline, cubic, whatever
+
+    return fs / mean(diff(crossings))
+
+
+def freq_from_fft(sig, fs):
+    """
+    Estimate frequency from peak of FFT
+    """
+    # Compute Fourier transform of windowed signal
+    windowed = sig * signal.blackmanharris(len(sig))
+    f = rfft(windowed)
+
+    # Find the peak and interpolate to get a more accurate peak
+    i = argmax(abs(f))  # Just use this for less-accurate, naive version
+    true_i = parabolic(log(abs(f)), i)[0]
+
+    # Convert to equivalent frequency
+    return fs * true_i / len(windowed)
+
+
+def freq_from_autocorr(sig, fs):
+    """
+    Estimate frequency using autocorrelation
+    """
+    # Calculate autocorrelation and throw away the negative lags
+    corr = correlate(sig, sig, mode='full')
+    corr = corr[len(corr)//2:]
+
+    # Find the first low point
+    d = diff(corr)
+    start = nonzero(d > 0)[0][0]
+
+    # Find the next peak after the low point (other than 0 lag).  This bit is
+    # not reliable for long signals, due to the desired peak occurring between
+    # samples, and other peaks appearing higher.
+    # Should use a weighting function to de-emphasize the peaks at longer lags.
+    peak = argmax(corr[start:]) + start
+    px, py = parabolic(corr, peak)
+
+    return fs / px
+
+
+def freq_from_HPS(sig, fs):
+    """
+    Estimate frequency using harmonic product spectrum (HPS)
+    """
+    windowed = sig * signal.blackmanharris(len(sig))
+
+    from pylab import subplot, plot, log, copy, show
+
+    # harmonic product spectrum:
+    c = abs(rfft(windowed))
+    maxharms = 4
+    # subplot(maxharms, 1, 1)
+    # plot(log(c))
+    for x in range(2, maxharms):
+        a = copy(c[::x])  # Should average or maximum instead of decimating
+        # max(c[::x],c[1::x],c[2::x],...)
+        c = c[:len(a)]
+        i = argmax(abs(c))
+        true_i = parabolic(abs(c), i)[0]
+        print('Pass %d: %f Hz' % (x, fs * true_i / len(windowed)))
+        c *= a
+        # subplot(maxharms, 1, x)
+        # plot(log(c))
+    # show()
+    return fs * true_i / len(windowed)
+
+
+
 
 
 if __name__ == '__main__':
